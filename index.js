92ef95900bb8cfd2c24f5a1715294b02fdd8eb97
loedroptaxi-del <leodroptaxi@gmail.com>
2025-12-30 15:24:02 +0530

    dec30-commit-6

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   origin: [
+    "http://localhost:5173",
     "https://insightsconsult-frontend.onrender.com"
   ], // frontend URL
   credentials: true
