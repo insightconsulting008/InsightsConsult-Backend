a35f21122433a505fc186860b918d981a8d8ce21
loedroptaxi-del <leodroptaxi@gmail.com>
2026-03-10 11:30:29 +0530

    mar-10 2st-commit

--- src/paymentSetting/PaymentSetting.js
+++ src/paymentSetting/PaymentSetting.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const prisma = require("../prisma/prisma");
-
 const detectMode = require("./detectMode");
 const createWebhook = require("./createWebhook");
 const {authenticate,authorizeRoles} =require("../authMiddleware/authMiddleware")
