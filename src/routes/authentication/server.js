9e3b3877fa24aef870cf82aece65b3b1929f9076
loedroptaxi-del <leodroptaxi@gmail.com>
2026-04-19 11:51:53 +0530

    this is where the folder structure happen

--- src/routes/authentication/server.js
+++ src/routes/authentication/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-router.use("/",        require("./auth.routes"));        // refresh, logout
+router.use("/V1",        require("./auth.routes"));        // refresh, logout
 router.use("/setup",        require("./adminSetup.routes"));        // refresh, logout
 router.use("/staff",  require("./staff.auth.routes"));  // staff login
 router.use("/user",   require("./user.auth.routes"));   // user login
